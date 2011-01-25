5ccb14f76263f9751743709cfdc72e89dee9a43f
joseferminj <joseferminj@air.local>
2011-01-25 08:52:41 +0100

    Exercise 2.06 Church numerals. Incomplete. The addition is not implemented

---
+++ 02_Building_abstractions_with_data/01_Introduction_to_data_abstractions/2_06.scm
@@ -0,0 +1,34 @@
+;; *Exercise 2.6:* In case representing pairs as procedures wasn't
+;; mind-boggling enough, consider that, in a language that can
+;; manipulate procedures, we can get by without numbers (at least
+;; insofar as nonnegative integers are concerned) by implementing 0
+;; and the operation of adding 1 as
+;;      (define zero (lambda (f) (lambda (x) x)))
+;;
+;;      (define (add-1 n)
+;;        (lambda (f) (lambda (x) (f ((n f) x)))))
+;;  This representation is known as "Church numerals", after its
+;; inventor, Alonzo Church, the logician who invented the [lambda]
+;; calculus.
+
+;; Define `one' and `two' directly (not in terms of `zero' and
+;; `add-1').  (Hint: Use substitution to evaluate `(add-1 zero)').
+;; Give a direct definition of the addition procedure `+' (not in
+;; terms of repeated application of `add-1').
+
+(define zero
+  (lambda (f)
+    (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f)
+    (lambda (x) (f ((n f) x)))))
+
+(define one
+  (lambda (f)
+    (lambda (x) (f ((lambda (z) z) f) x))))
+
+(define two
+  (lambda (f)
+    (lambda (x) (f ((lambda (o) (f ((lambda (z) z) f) z)) f) x))))
+
