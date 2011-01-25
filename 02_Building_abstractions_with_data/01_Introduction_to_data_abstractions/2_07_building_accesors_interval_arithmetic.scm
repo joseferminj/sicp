d553142fc30c75e5ed94bcc4b1c70c58ba05770f
joseferminj <joseferminj@air.local>
2011-01-25 09:05:50 +0100

    Exercise 2.07 Selectors Interval Arithmetic

---
+++ 02_Building_abstractions_with_data/01_Introduction_to_data_abstractions/2_07_building_accesors_interval_arithmetic.scm
@@ -0,0 +1,13 @@
+;; Exercise 2.7.  Alyssa's program is incomplete because she has not specified the implementation of the interval abstraction. Here is a definition of the interval constructor:
+
+(define (make-interval a b) (cons a b))
+
+;; Define selectors upper-bound and lower-bound to complete the implementation.
+
+(define (lower-bound i)
+  (car i))
+
+(define (upper-bound i)
+  (cdr i))
+
+
