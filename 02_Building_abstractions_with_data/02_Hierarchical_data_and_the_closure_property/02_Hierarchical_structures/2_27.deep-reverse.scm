fd390f5da45d6e66778d2540827d6107e8126685
joseferminj <joseferminj@gmail.com>
2011-01-30 15:57:36 +0100

    Exercise 2.27 Deep reverse procedure

---
+++ 02_Building_abstractions_with_data/02_Hierarchical_data_and_the_closure_property/02_Hierarchical_structures/2_27.deep-reverse.scm
@@ -0,0 +1,25 @@
+;; Exercise 2.27.  Modify your reverse procedure of exercise 2.18 to
+;; produce a deep-reverse procedure that takes a list as argument and
+;; returns as its value the list with its elements reversed and with
+;; all sublists deep-reversed as well. For example,
+
+;; (define x (list (list 1 2) (list 3 4)))
+
+;; x
+;; ((1 2) (3 4))
+
+;; (reverse x)
+;; ((3 4) (1 2))
+
+;; (deep-reverse x)
+;; ((4 3) (2 1))
+
+;;; Solution
+;;; ========
+
+(define (deep-reverse l)
+  (define (r l1 l2)
+    (cond [(null? l1) l2]
+          [(pair? (car l1)) (r (cdr l1) (cons (deep-reverse (car l1)) l2))]
+          [else (r (cdr l1) (cons (car l1) l2))]))
+  (r l '()))
