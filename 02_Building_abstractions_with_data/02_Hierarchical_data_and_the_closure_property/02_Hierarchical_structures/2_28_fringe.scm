51fd2f4efbca3318df4d757cc924f72d5d35f362
joseferminj <joseferminj@gmail.com>
2011-01-30 16:08:16 +0100

    Exercise 2.28 Fringe procedure

---
+++ 02_Building_abstractions_with_data/02_Hierarchical_data_and_the_closure_property/02_Hierarchical_structures/2_28_fringe.scm
@@ -0,0 +1,17 @@
+;; Exercise 2.28.  Write a procedure fringe that takes as argument a
+;; tree (represented as a list) and returns a list whose elements are
+;; all the leaves of the tree arranged in left-to-right order. For
+;; example,
+
+;; (define x (list (list 1 2) (list 3 4)))
+
+;; (fringe x)
+;; (1 2 3 4)
+
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+
+(define (fringe t)
+  (cond [(null? t) '()]
+        [(not (pair? t)) (list t)]
+        [else (append (fringe (car t)) (fringe (cdr t)))]))
